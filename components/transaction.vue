238f0c77e9c7f676e6874f95121da3f5d421ede0
ngtinn59 <tinb2004814@student.ctu.edu.vn>
2025-01-20 20:51:11 +0700

    Feat: Transsaction

--- components/transaction.vue
+++ components/transaction.vue
@@ -1,14 +1,44 @@
 <template>
-    <div class="grid grid-cols-2">
-        <div class="flex items-center space-x-1">
-            <UIcon name="i-heroicons-arrow-up-right" class="text-green-600" />
+    <div class="grid grid-cols-2 py-4 border-b drak:border-gray-800 text-gray-900 dark:text-gray-100">
+        <!-- Left Section: Salary Info -->
+        <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-1">
+                <UIcon name="i-heroicons-arrow-up-right" class="text-green-600" />
+                <div>Salary</div>
+            </div>
+            <div>
+                <UBadge color="white">Category</UBadge>
+            </div>
         </div>
-        <div>Category</div>
-    </div>
-    <div class="flex items-center justify-end space-x-2">
-        <div>3000$</div>
-        <div>
-            <button>Options</button>
+
+        <!-- Right Section: Currency and Options -->
+        <div class="flex items-center justify-end space-x-2">
+            <div>{{ currency }}</div>
+            <div>
+                <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+                    <UButton color="white" variant="ghost" trailing-icon="i-heroicons-ellipsis-horizontal"></UButton>
+                </UDropdown>
+            </div>
         </div>
     </div>
 </template>
+
+<script setup>
+const { currency } = useCurrency(3000);
+
+const items = [
+    [
+        {
+            label: 'Edit',
+            icon: 'i-heroicons-pencil',
+            click: () => console.log('Edit')
+
+        },
+        {
+            label: 'Delete',
+            icon: 'i-heroicons-trash',
+            click: () => console.log('Delete')
+        }
+    ]
+]
+</script>
