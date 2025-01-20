7bb3b49da93e6f6cb9bba33e2a2c481c4da744c8
ngtinn59 <tinb2004814@student.ctu.edu.vn>
2025-01-20 11:34:48 +0700

    update

---
+++ components/trend.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="font-bold" :class="[color]"> {{ title }}</div>
+        <div class="text-2xl font-extrabold text-black drak:text-white mb2">
+            <USkeleton v-if="loading" class="h-8 w-24" />
+            <div v-else>{{ amount }}</div>
+        </div>
+   </div>
+   <div>
+    <USkeleton v-if="loading" class="h-4 w-24" />
+    <div class="flex space-x-1 items-center text-sm">
+        <UIcon name="i-heroicons:arrow-trending-up" class="w-6 h-6" :class="[color]"/>
+    </div>
+   </div>
+</template>
+
+<script setup>
+
+defineProps ({
+    color: String,
+    title: String,
+    amount: Number,
+    lastAmount: Number,
+    loading: Boolean
+})
+</script>
+
+<style scoped>
+.green {
+    @apply text-green-600 dark:text-green-400;
+}
+.red {
+    @apply text-red-600 dark:text-red-400;
+}
+</style>
