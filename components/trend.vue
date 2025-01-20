d4df15edb6160fc2dae9452966d6b36d0c76767c
ngtinn59 <tinb2004814@student.ctu.edu.vn>
2025-01-20 11:34:56 +0700

    updateee

--- components/trend.vue
+++ components/trend.vue
@@ -3,32 +3,61 @@
         <div class="font-bold" :class="[color]"> {{ title }}</div>
         <div class="text-2xl font-extrabold text-black drak:text-white mb2">
             <USkeleton v-if="loading" class="h-8 w-24" />
-            <div v-else>{{ amount }}</div>
+            <div v-else>{{ currency }}</div>
         </div>
-   </div>
-   <div>
-    <USkeleton v-if="loading" class="h-4 w-24" />
-    <div class="flex space-x-1 items-center text-sm">
-        <UIcon name="i-heroicons:arrow-trending-up" class="w-6 h-6" :class="[color]"/>
     </div>
-   </div>
+    <div>
+        <USkeleton v-if="loading" class="h-4 w-24" />
+        <div class="flex space-x-1 items-center text-sm">
+            <UIcon :name="icon" class="w-6 h-6" :class="{ 'green': trending, 'red': !trending }" />
+        </div>
+        <div class="text-gray-500 dark:text-gray-400">
+            {{ percentageTrend }}
+        </div>
+    </div>
 </template>
 
 <script setup>
 
-defineProps ({
+const props = defineProps({
     color: String,
     title: String,
     amount: Number,
     lastAmount: Number,
     loading: Boolean
 })
+
+const trending = computed(
+    () => props.amount >= props.lastAmount
+)
+
+const icon = computed(
+    () => trending.value ? 'i-heroicons:arrow-trending-up' : 'i-heroicons:arrow-trending-down'
+)
+
+const { currency } = useCurrency(props.amount);
+
+
+const percentageTrend = computed(() => {
+    if (props.amount === 0 || props.lastAmount === 0) {
+        return '0%';
+    }
+    
+    const bigger = Math.max(props.amount, props.lastAmount);
+    const lower = Math.min(props.amount, props.lastAmount);
+    const ratio = ((bigger - lower) / lower) * 100;
+    console.log(ratio, bigger, lower);
+    return `${ratio.toFixed(2)}%`;
+});
+
+
 </script>
 
 <style scoped>
 .green {
     @apply text-green-600 dark:text-green-400;
 }
+
 .red {
     @apply text-red-600 dark:text-red-400;
 }
---
+++ composables/useCurrency.js
@@ -0,0 +1,14 @@
+import { computed, isRef } from 'vue';
+
+export const useCurrency = (amount) => {
+    const currency = computed(() => {
+        return new Intl.NumberFormat('vi-VN', { 
+            style: 'currency', 
+            currency: 'VND' 
+        }).format(isRef(amount) ? amount.value : amount);
+    });
+
+    return {
+        currency  
+    };
+};
