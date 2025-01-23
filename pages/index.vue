61afa73bec1f94c70355d8205340bb90ff28c102
ngtinn59 <tinb2004814@student.ctu.edu.vn>
2025-01-23 15:19:55 +0700

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -16,16 +16,44 @@
   </section>
 
   <section>
-    <Transaction />
-    <Transaction />
-    <Transaction />
-    <Transaction /> 
+    <Transaction v-for="transaction in transactions" :key="transaction.id" :transaction="transaction" />
   </section>
 </template>
 
 <script setup>
 import { transactionViewOptions } from '~/constants';
+import { createClient } from '@supabase/supabase-js';
 
 
 const viewSelected = ref(transactionViewOptions[1])
+const supabaseUrl = 'https://zysxaeapivqgnebevfcn.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5c3hhZWFwaXZxZ25lYmV2ZmNuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNzM4MjExNywiZXhwIjoyMDUyOTU4MTE3fQ.ltNgTfYU3xECpQEAyn008W0lQNlmYpCUAsG5Nfrmq3I'; // Sử dụng anon key từ Supabase Dashboard
+const supabase = createClient(supabaseUrl, supabaseKey);
+const transactions = ref([]);
+const {data, pending} = await useAsyncData('transactions', async () => {
+  const { data, error } = await supabase
+    .from('transactions')
+    .select('*')
+    .order('created_at', { ascending: false });
+  if (error) {
+    throw error;
+  }
+  return data;
+});
+
+transactions.value = data.value;
+
+const transactionsGrouped = computed(() => {
+  let grouped = {}
+
+  for (const transaction of transactions.value) {
+    const date = new Date(transaction.created_at).toISOString().split('T')[0];
+    if (!grouped[date]) {
+      grouped[date] = [];
+    }
+    grouped[date].push(transaction);
+  }
+})
+
+console.log(transactionsGrouped.value)
 </script>
