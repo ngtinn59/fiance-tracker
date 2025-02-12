9a4d5b18d9e5b7bc147cad60da8eccfc636d58fb
ngtinn59 <tinb2004814@student.ctu.edu.vn>
2025-02-12 12:07:59 +0700

    Forms Schema Validation with the Zod Library

--- components/transaction-modal.vue
+++ components/transaction-modal.vue
@@ -2,8 +2,9 @@
     <UModal v-model="isOpen">
         <UCard>
             <template #header>
+                Add Transaction
             </template>
-            <UForm :state="state">
+            <UForm :state="state" :schema="schema" ref="form" @submit.prevent="save">
                 <UFormGroup :required="true" label="Transaction Type" name="type" class="mb-4">
                     <USelect placeholder="Transaction Type" :options="types" v-model="state.type"/>
                 </UFormGroup>
@@ -35,16 +36,50 @@
 
 <script setup>
 import { categories, types } from '~/constants';
-
+import {z} from 'zod';
 const props = defineProps({
     modelValue: Boolean
 });
 
 const emit = defineEmits(['update:modelValue']);
+const defaultSchema = z.object({
+    type: z.string().nonempty(),
+    amount: z.number().positive(),
+    created_at: z.string().nonempty(),
+    description: z.string().optional(),
+    category: z.string().nonempty()
+})
 
+const incomeSchema = z.object({
+    type: z.literal('Income')
+})
+
+const expenseSchema = z.object({
+    type: z.literal('Expense'),
+    category: z.enum(categories)
+})
+
+const investmentSchema = z.object({
+    type: z.literal('Investment')
+})
+
+const savingSchema = z.object({
+    type: z.literal('Saving')
+})
+const schema = z.intersection(
+  defaultSchema,
+  z.discriminatedUnion('type', [incomeSchema, expenseSchema, investmentSchema, savingSchema])
+);
+
+
+const form = ref()
+
+const save = async () => {
+    form.value.validate()
+}
 const state = ref({
     type: undefined,
-    amount: undefined,
+    amount: 0,
     created_at: undefined,
     description: undefined,
     category: undefined
